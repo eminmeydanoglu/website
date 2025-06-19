428a19b22fe7c40e26fa84ed4746c5757c8d0257
meydanoglu <eminmeydanoglu@gmail.com>
2025-06-19 21:13:19 +0300

    Add CV page and update navigation links to include CV

---
+++ app/cv/page.tsx
@@ -0,0 +1,20 @@
+import { genPageMetadata } from 'app/seo'
+
+export const metadata = genPageMetadata({ title: 'CV' })
+
+export default function CV() {
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          CV
+        </h1>
+      </div>
+      <div className="container py-12">
+        <div className="-m-4 flex flex-wrap">
+          <iframe src="/static/cv.pdf" width="100%" height="800px" />
+        </div>
+      </div>
+    </div>
+  )
+}
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'all writings' },
   { href: '/projects', title: 'projects' },
-  { href: '/about', title: 'about' },
+  { href: '/cv', title: 'CV' },
 ]
 
 export default headerNavLinks
